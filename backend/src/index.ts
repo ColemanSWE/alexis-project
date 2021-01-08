853d2f6492b51759859dac52f6107bfb5d26bae0
Coleman Rose <colemanrose.new@gmail.com>
2021-01-08 14:42:27 +0100

    Creating a users Model for mongoose

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,19 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import { json } from 'body-parser'
 import { userRouter } from './routes/users'
 
 const app = express()
 app.use(json())
 app.use(userRouter)
+
+mongoose.connect('mongodb://localhost:27017/users', {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => {
+    console.log('connected to database')
+})
 
 app.listen(3000, () => {
     console.log('server is listening on port 3000')
