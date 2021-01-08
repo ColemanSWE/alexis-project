729a6dc5aa031e799c3bebe2bdf5b580662266f7
Coleman Rose <colemanrose.new@gmail.com>
2021-01-08 14:06:34 +0100

    Adding type to requests and response

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,12 +1,12 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 
 const router = express.Router()
 
-router.get('/api/user', (req, res) => {
+router.get('/api/user', (req: Request, res: Response) => {
     return res.send('the user')
 })
 
-router.post('api/user', (req, res) => {
+router.post('api/user', (req: Request, res: Response) => {
     return res.send('new user created')
 })
 
