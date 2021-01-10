2772d11094c2552261dcfef1cf4f4d6bf7a9e9c8
Coleman Rose <colemanrose.new@gmail.com>
2021-01-10 20:03:11 +0100

    Yay it works, time to add user in the corner and style it.

--- frontend/src/ListItem.tsx
+++ frontend/src/ListItem.tsx
@@ -1,5 +1,22 @@
 import React from "react";
 
-export default function ListItem() {
-  return <div>ListItem</div>;
+interface Props {
+  avatar: {
+    url: string
+  }
+  name: {
+    first: string
+    last: string
+  }
+  email?: string
+  id?: string
+}
+
+export const ListItem: React.FC<Props> = ({avatar, name}) => {
+  return (
+    <div className="list-item">
+      <div>{name.first} {name.last}</div>
+      <div><img alt="User avatar" src={avatar.url} /></div>
+    </div>
+  )
 }
