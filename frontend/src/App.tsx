7251e199d4e5a4f6a5cbcf807d076f10f728d034
Coleman Rose <colemanrose.new@gmail.com>
2021-01-13 08:00:38 +0100

    App is still ugly but I have a working filter. :)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.scss";
 import List from "./List";
 import { Me } from './Me'
--- frontend/src/List.tsx
+++ frontend/src/List.tsx
@@ -4,6 +4,7 @@ import { ListItem } from './ListItem';
 
 interface IUserListState {
   userList: Array<any>
+  filterString: string
 }
 
 interface User {
@@ -21,7 +22,9 @@ interface User {
 export default class List extends Component<{}, IUserListState> {
   constructor(props: IUserListState) {
     super(props)
-    this.state = { userList: [] }
+    this.state = { userList: [], filterString: '' }
+
+    this.handleChange = this.handleChange.bind(this)
   }
 
   public componentWillMount(): void {
@@ -32,14 +35,26 @@ export default class List extends Component<{}, IUserListState> {
     })
   }
 
+  public handleChange(event: React.ChangeEvent<HTMLInputElement>): void {
+    this.setState({ filterString: event.target.value })
+  }
+
   render() {
     return (
-      <div className="list-container">
-        <ul>
-          {this.state.userList.map((user: User) => {
-            return <ListItem key={user.id} avatar={user.avatar} name={user.name} />
-          })}
-        </ul>
+      <div>
+        <div className="list-container">
+        <div>
+          <input className="filter" type="text" onChange={this.handleChange}/>
+        </div>
+          <ul>
+            {this.state.userList.filter((user: User)=> {
+              const fullName = user.name.first + user.name.last
+              return fullName.toLowerCase().includes(this.state.filterString.toLowerCase())
+            }).map((user: User) => {
+              return <ListItem key={user.id} avatar={user.avatar} name={user.name} />
+            })}
+          </ul>
+        </div>
       </div>
     )
   }
