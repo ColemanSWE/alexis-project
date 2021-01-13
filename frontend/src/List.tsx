37f97765ca7ecada9d6c3510d613f910728a5b1a
Coleman Rose <colemanrose.new@gmail.com>
2021-01-13 10:11:11 +0100

    Comments added for clarity.

--- frontend/src/List.tsx
+++ frontend/src/List.tsx
@@ -28,6 +28,7 @@ export default class List extends Component<{}, IUserListState> {
   }
 
   public componentWillMount(): void {
+    // When I point this towards the written API it seems that this lifecycle method breaks (listed as unstable in this version of React)
     const apiUrl = 'https://jonruna.github.io/tapi/userList.json'
     axios.get(apiUrl).then(response => {
       this.setState({ userList: response.data.userList })
@@ -43,12 +44,17 @@ export default class List extends Component<{}, IUserListState> {
     return (
       <div>
         <div className="list-container">
-        <div>
-          <input className="filter" type="text" onChange={this.handleChange}/>
-        </div>
+          <label htmlFor="name" className="label-name">
+            <span className="content-name">Filter</span>
+          </label>
+          <div>
+            <input className="filter" type="text" onChange={this.handleChange}/>
+          </div>
           <ul>
+            {/* The magic happens here. Text is filtered using the first and last name data and the onChange function of the input field and the 
+            list is dynamically generated from there.*/}
             {this.state.userList.filter((user: User)=> {
-              const fullName = user.name.first + user.name.last
+              const fullName = user.name.first + ' ' + user.name.last
               return fullName.toLowerCase().includes(this.state.filterString.toLowerCase())
             }).map((user: User) => {
               return <ListItem key={user.id} avatar={user.avatar} name={user.name} />
